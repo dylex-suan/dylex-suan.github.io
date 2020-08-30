60b05075307eab677bc0a60d004c28666708172f
Kristian Dylex Suan <dylexms@gmail.com>
2020-08-30 14:25:52 -0400

    Added js file

---
+++ mainjs.js
@@ -0,0 +1,24 @@
+/* NAVIGATION SITE -- MENU BUTTON */
+
+// Used to connect with the mainpage.html file
+// When user clicks on the button, we can toggle between hiding and showing the dropdown content
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onClick = function (event) {
+    // If the event DOES not match the target for the dropbutton...
+    if (!event.target.matches('.dropbtn')) {
+        // Then we get the 'dropdown-content' class
+        var dropdowns = document.getElementsByClassName('dropdown-content');
+        var i;
+        // Next, e
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
