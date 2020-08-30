06b610a4355b3e5d5feca908a91292d7b60beb04
Kristian Dylex Suan <dylexms@gmail.com>
2020-08-30 14:29:41 -0400

    Added mainjs.js and fixed some comments

--- mainjs.js
+++ mainjs.js
@@ -8,18 +8,12 @@ function myFunction() {
 
 // Close the dropdown menu if the user clicks outside of it
 window.onClick = function (event) {
-<<<<<<< HEAD
-    if (!event.target.matches('.dropbtn')) {
-        var dropdowns = document.getElementsByClassName ('dropdown-content');
-        var i;
-=======
     // If the event DOES not match the target for the dropbutton...
     if (!event.target.matches('.dropbtn')) {
         // Then we get the 'dropdown-content' class
         var dropdowns = document.getElementsByClassName('dropdown-content');
         var i;
         // Next, e
->>>>>>> work_on_body
         for (i = 0; i < dropdowns.length; i++) {
             var openDropdown = dropdowns[i];
             if (openDropdown.classList.contains('show')) {
